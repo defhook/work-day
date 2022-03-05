250049bd1d3afa0fb0c096d023dbaf15155c9fbc
Alvin Badillo <defhook152@gmail.com>
2022-03-05 12:14:28 -0700

    addition of getting localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 
 var displayDate = function() {
-    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")); 
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY")); 
     //document.getElementById("currentDay").textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-   
 }
+
+var timeArray = [
+    "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"
+]
 
 
 //call function
@@ -20,7 +23,16 @@ $(".saveBtn").on('click', function(){
     //save information in localStorage
     localStorage.setItem(timeStamp,descriptionSection); 
 });
+for (let i=0; timeArray.length; i++){
+    console.log(timeArray[i])
+    console.log(localStorage.getItem(timeArray[i]))
+}
+
+
+
 
 //page load event
 var localStorageVal = localStorage.getItem("9:00 AM"); 
-console.log(localStorageVal); 
+// console.log(localStorageVal); 
+console.log(localStorageVal)
+
